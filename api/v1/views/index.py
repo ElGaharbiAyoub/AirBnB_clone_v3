f0d224132a7af20ff62727784f7c14b63ff3aa20
amasin76 <amasin76@gmail.com>
2023-10-27 18:08:07 +0000

    Add: stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """index api"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
@@ -11,3 +12,16 @@ def status():
       "status": "OK"
     }
     return jsonify(data)
+
+
+@app_views.route('/stats')
+def stats():
+    """Retrieves the number of each objects by type"""
+    return jsonify({
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User')
+    })

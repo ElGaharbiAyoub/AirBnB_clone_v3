50405cd37aa3f66e347c4cb972c51f0e3f18f777
amasin76 <amasin76@gmail.com>
2023-10-30 09:16:10 +0000

    Add: `get` & `count` tests for file_storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -132,3 +132,25 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_get(self):
+        """Tests method for obtaining an instance file storage"""
+        storage = FileStorage()
+        instance = State(name="FState")
+        storage.new(instance)
+        storage.save()
+        get_instance = storage.get(State, instance.id)
+        self.assertEqual(get_instance, instance)
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_count(self):
+        """Tests count method file storage"""
+        storage = FileStorage()
+        state = State(name="FFState")
+        storage.new(state)
+        city = City(name="FCity")
+        storage.new(city)
+        storage.save()
+        c = storage.count()
+        self.assertEqual(len(storage.all()), c)
